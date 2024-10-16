6991973a468ce5dad579f4807438521c6919f9a3
duyanhhh1811 <duyanhlucas302@gmail.com>
2024-10-16 23:58:07 +0700

    feat(auth): add validation for Register Page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,46 +1,68 @@
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { authRules } from '../../utils/authRules'
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues
+  } = useForm()
+
+  const handleRegisterSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
+  console.log(errors)
+
   return (
     <div className='bg-shopee_orange'>
       <div className='max-w-7xl mx-auto px-4'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='p-10 rounded bg-white shadow-sm'>
+            <form className='p-10 rounded bg-white shadow-sm' noValidate onSubmit={handleRegisterSubmit}>
               <div className='text-2xl'>Đăng ký</div>
               <div className='mt-8'>
                 <input
                   type='email'
-                  name='email'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Email'
+                  {...register('email', authRules.email)}
                 />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors?.email?.message as string}</div>
               </div>
               <div className='mt-3'>
                 <input
                   type='password'
-                  name='password'
+                  autoComplete='on'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Password'
+                  {...register('password', authRules.password)}
                 />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors?.password?.message as string}</div>
               </div>
               <div className='mt-3'>
                 <input
                   type='password'
-                  name='confirm_password'
+                  autoComplete='on'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Confirm Password'
+                  {...register('confirm_password', {
+                    ...authRules.confirm_password,
+                    validate: (value) => value === getValues('password') || 'Password không khớp'
+                  })}
                 />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>
+                  {errors?.confirm_password?.message as string}
+                </div>
               </div>
-              <div className='mt-3'>
+              <div className='mt-8'>
                 <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
                   Đăng ký
                 </button>
               </div>
-              <div className='flex items-center justify-center mt-8'>
+              <div className='flex items-center justify-center mt-5'>
                 <span className='text-gray-400'>Bạn đã có tài khoản?</span>
                 <Link className='text-red-400 ml-1' to='/login'>
                   Đăng nhập
---
+++ src/utils/authRules.ts
@@ -0,0 +1,44 @@
+import { type RegisterOptions } from 'react-hook-form'
+
+type CustomAuthRule = { [key in 'email' | 'password' | 'confirm_password']: RegisterOptions }
+
+export const authRules: CustomAuthRule = {
+  email: {
+    required: {
+      value: true,
+      message: 'Email không được rỗng'
+    },
+    pattern: {
+      value: /^\S+@\S+\.\S+$/,
+      message: 'Email không đúng định dạng'
+    },
+    minLength: {
+      value: 5,
+      message: 'Email có độ dài từ 5 - 160 ký tự'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Email có độ dài từ 5 - 160 ký tự'
+    }
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'Password không được rỗng'
+    },
+    minLength: {
+      value: 6,
+      message: 'Password có độ dài từ 6 - 160 ký tự'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Password có độ dài từ 6 - 160 ký tự'
+    }
+  },
+  confirm_password: {
+    required: {
+      value: true,
+      message: 'Confirm Password không được rỗng'
+    }
+  }
+}
