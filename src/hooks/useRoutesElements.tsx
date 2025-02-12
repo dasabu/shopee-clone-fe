9c0c2d6f8f4d549cc303a04f88908411c2e3ca90
duyanhhh1811 <duyanhlucas302@gmail.com>
2025-02-12 14:01:00 +0700

    feat: projected and rejected routes

--- src/hooks/useRoutesElements.tsx
+++ src/hooks/useRoutesElements.tsx
@@ -1,14 +1,27 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import AuthLayout from '@/layouts/AuthLayout'
 import MainLayout from '@/layouts/MainLayout'
 import Login from '@/pages/Login'
 import ProductList from '@/pages/ProductList'
 import Register from '@/pages/Register'
+import Profile from '@/pages/Profile'
+
+const isAuthenticated = false
+
+function ProtectedRoute() {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+// Chưa login
+function RejectedRoute() {
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRoutesElements() {
   const routesElements = useRoutes([
     {
       path: '/',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
@@ -16,20 +29,40 @@ export default function useRoutesElements() {
       )
     },
     {
-      path: '/login',
-      element: (
-        <AuthLayout>
-          <Login />
-        </AuthLayout>
-      )
+      path: '/',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/register',
-      element: (
-        <AuthLayout>
-          <Register />
-        </AuthLayout>
-      )
+      path: '/',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/login',
+          element: (
+            <AuthLayout>
+              <Login />
+            </AuthLayout>
+          )
+        },
+        {
+          path: '/register',
+          element: (
+            <AuthLayout>
+              <Register />
+            </AuthLayout>
+          )
+        }
+      ]
     }
   ])
   return routesElements
