d3981a3cb9465f1ea6b50371bbb9b5ba5100ca96
duyanhhh1811 <duyanhlucas302@gmail.com>
2025-02-25 05:04:14 +0700

    apis and types for Purchase module

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,3 +9,13 @@ export const ORDER = {
   ASC: 'asc',
   DESC: 'desc'
 } as const
+
+export const PURCHASE_STATUS = {
+  inCart: -1,
+  all: 0,
+  waitForConfirmation: 1,
+  waitForGetting: 2,
+  inProgress: 3,
+  delivered: 4,
+  cancelled: 5
+} as const
